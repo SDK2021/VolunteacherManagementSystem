d9442bcfbbf1ef73ed7e1bdd58f29fece5ce2028
SDK2021 <77728099+SDK2021@users.noreply.github.com>
2021-02-06 19:31:10 +0530

    Updated By Suhanee
    Imported CascadeType

--- Taluka.java
+++ Taluka.java
@@ -10,6 +10,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.CascadeType;
 
 @Entity
 public class Taluka {
