fd25174af6e9980c3669dc1fc49505862f723d59
SDK2021 <77728099+SDK2021@users.noreply.github.com>
2021-02-07 20:17:27 +0530

    Update Event.java

--- VolunteacherManagementSystem/src/main/java/com/volunteacher/app/Model/Event.java
+++ VolunteacherManagementSystem/src/main/java/com/volunteacher/app/Model/Event.java
@@ -14,7 +14,7 @@ import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import com.sun.istack.NotNull;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Event {
