5421bb2cb8343d055589d6e7e1e682ffe80c9bf8
SDK2021 <77728099+SDK2021@users.noreply.github.com>
2021-02-07 20:19:59 +0530

    Update SessionReport.java

--- VolunteacherManagementSystem/src/main/java/com/volunteacher/app/Model/SessionReport.java
+++ VolunteacherManagementSystem/src/main/java/com/volunteacher/app/Model/SessionReport.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 
-import com.sun.istack.NotNull;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class SessionReport {
