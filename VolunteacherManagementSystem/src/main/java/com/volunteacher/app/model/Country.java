13525460229eb51c14dfd3109135b47f775e3d72
SDK2021 <sdkproject2021@gmail.com>
2021-02-11 20:45:13 +0530

    Revert "Revert "Merge branch 'main' of https://github.com/SDK2021/VolunteacherManagementSystem into main""
    This reverts commit 70c7d759fdbfb0f5c2b0b3be679b9d98ac5927e9.

--- VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/Country.java
+++ VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/Country.java
@@ -20,9 +20,46 @@ public class Country {
 	private int countryId;
 
 	@NotNull
-	@Column(length = 20, nullable = false, unique = true , columnDefinition = "Char")
+	@Column(length = 20, nullable = false, unique = true, columnDefinition = "Char")
 	private String countryName;
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "country")
 	private List<State> states;
+
+	
+	public Country() {
+		super();	
+	}
+
+	public Country(@NotNull String countryName, List<State> states) {
+		super();
+		this.countryName = countryName;
+		this.states = states;
+	}
+
+	public int getCountryId() {
+		return countryId;
+	}
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
+
+	public List<State> getStates() {
+		return states;
+	}
+
+	public void setStates(List<State> states) {
+		this.states = states;
+	}
+
+	@Override
+	public String toString() {
+		return "Country [countryId=" + countryId + ", countryName=" + countryName + ", states=" + states + "]";
+	}
+	
 }
--- VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/State.java
+++ VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/State.java
@@ -21,7 +21,7 @@ public class State {
 	private int stateId;
 
 	@NotNull
-	@Column(length = 20, unique = true, nullable = false , columnDefinition = "Char")
+	@Column(length = 20, unique = true, nullable = false, columnDefinition = "Char")
 	private String stateName;
 
 	@NotNull
@@ -30,4 +30,51 @@ public class State {
 
 	@OneToMany(cascade = CascadeType.ALL, mappedBy = "state")
 	private List<District> districts;
+
+	
+	public State() {
+		super();
+	}
+
+	public State(@NotNull String stateName, @NotNull Country country, List<District> districts) {
+		super();
+		this.stateName = stateName;
+		this.country = country;
+		this.districts = districts;
+	}
+
+	public int getStateId() {
+		return stateId;
+	}
+
+	public String getStateName() {
+		return stateName;
+	}
+
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
+
+	public Country getCountry() {
+		return country;
+	}
+
+	public void setCountry(Country country) {
+		this.country = country;
+	}
+
+	public List<District> getDistricts() {
+		return districts;
+	}
+
+	public void setDistricts(List<District> districts) {
+		this.districts = districts;
+	}
+
+	@Override
+	public String toString() {
+		return "State [stateId=" + stateId + ", stateName=" + stateName + ", country=" + country + ", districts="
+				+ districts + "]";
+	}
+	
 }
