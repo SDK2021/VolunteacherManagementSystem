445630656276d467c56e0c4c0e1f864c98673116
SDK2021 <77728099+SDK2021@users.noreply.github.com>
2021-02-16 17:59:02 +0530

    Update KidsReport.java

--- VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/KidsReport.java
+++ VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/KidsReport.java
@@ -24,7 +24,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 public class KidsReport {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	@Column(length = 6)
 	private long kidreportId;
 	
