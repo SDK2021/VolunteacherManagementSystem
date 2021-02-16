bc9f487da19f10a05e3a1b9a46959710a84765d9
SDK2021 <77728099+SDK2021@users.noreply.github.com>
2021-02-16 18:01:00 +0530

    Update School.java

--- VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/School.java
+++ VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/School.java
@@ -20,7 +20,7 @@ public class School {
 	
 	@Id
 	@Column(length = 3)
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private int schoolId;
 	
 	@NotNull
