ecc3e3aeb7433cf3236255c4e006b667f3b5d423
SDK2021 <77728099+SDK2021@users.noreply.github.com>
2021-02-16 18:02:32 +0530

    Update Village.java

--- VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/Village.java
+++ VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/Village.java
@@ -12,7 +12,7 @@ import javax.validation.constraints.NotNull;
 public class Village {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	@Column(length = 6)
 	private long villageId;
 
