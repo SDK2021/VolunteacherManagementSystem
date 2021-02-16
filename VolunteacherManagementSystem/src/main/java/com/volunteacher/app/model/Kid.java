226d2c4ae7b09bda0e6dc5ea0d05f4ad1bcd5c05
SDK2021 <77728099+SDK2021@users.noreply.github.com>
2021-02-16 17:58:06 +0530

    Update Kid.java

--- VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/Kid.java
+++ VolunteacherManagementSystem/src/main/java/com/volunteacher/app/model/Kid.java
@@ -20,7 +20,7 @@ import javax.validation.constraints.Size;
 public class Kid {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	@Column(length = 8)
 	private long kidId;
 	
